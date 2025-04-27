17e7311437424a6f8b11064db145223b6a226642
SowmyaVasudevan <vsowmya082000@gmail.com>
2025-04-27 19:56:04 +0530

    Fix before_install.sh to be a valid shell script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,3 @@
-hooks:
-  BeforeInstall:
-    - location: scripts/before_install.sh
-      timeout: 300
-      runas: root
-  AfterInstall:
-    - location: scripts/restart.sh
-      timeout: 300
-      runas: root
+#!/bin/bash
+echo "Running BeforeInstall hook"
+
